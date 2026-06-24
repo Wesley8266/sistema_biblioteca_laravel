fde95368070f49597f49a0224bfd393e1509e6c0
WESLEY DA SILVA ALMEIDA <wesleydasilvaalmeida706@gmail.com>
2026-06-24 16:56:20 -0300

    Colocando no docker e redirecionamento se não tiver logado no sistema

--- routes/web.php
+++ routes/web.php
@@ -8,25 +8,29 @@ use App\Http\Controllers\DashboardController;
 
 require __DIR__.'/auth.php';
 
-Route::get('/categorias', [CategoryController::class, 'index'])->name('categorias.index');
-Route::get('/categorias/criar', [CategoryController::class, 'create'])->name('categorias.create');
-Route::post('/categorias', [CategoryController::class, 'store'])->name('categorias.store');
-Route::get('/categorias/{id}/editar', [CategoryController::class, 'edit'])->name('categorias.edit');
-Route::put('/categorias/{id}', [CategoryController::class, 'update'])->name('categorias.update');
-Route::delete('/categorias/{id}', [CategoryController::class,'destroy'])->name('categorias.destroy');
+Route::middleware('auth')->group(function () {
 
-Route::get('/alunos', [AlunosController::class, 'index'])->name('alunos.index');
-Route::get('/alunos/criar', [AlunosController::class, 'create'])->name('alunos.create');
-Route::post('/alunos', [AlunosController::class, 'store'])->name('alunos.store');
-Route::get('/alunos/{id}/editar', [AlunosController::class, 'edit'])->name('alunos.edit');
-Route::put('/alunos/{id}', [AlunosController::class, 'update'])->name('alunos.update');
-Route::delete('/alunos/{id}', [AlunosController::class,'destroy'])->name('alunos.destroy');
+    Route::get('/categorias', [CategoryController::class, 'index'])->name('categorias.index');
+    Route::get('/categorias/criar', [CategoryController::class, 'create'])->name('categorias.create');
+    Route::post('/categorias', [CategoryController::class, 'store'])->name('categorias.store');
+    Route::get('/categorias/{id}/editar', [CategoryController::class, 'edit'])->name('categorias.edit');
+    Route::put('/categorias/{id}', [CategoryController::class, 'update'])->name('categorias.update');
+    Route::delete('/categorias/{id}', [CategoryController::class,'destroy'])->name('categorias.destroy');
 
-Route::get('/livros', [LivrosController::class, 'index'])->name('livros.index');
-Route::get('/livros/criar', [LivrosController::class, 'create'])->name('livros.create');
-Route::post('/livros', [LivrosController::class, 'store'])->name('livros.store');
-Route::get('/livros/{id}/editar', [LivrosController::class, 'edit'])->name('livros.edit');
-Route::put('/livros/{id}', [LivrosController::class, 'update'])->name('livros.update');
-Route::delete('/livros/{id}', [LivrosController::class, 'destroy'])->name('livros.destroy');
+    Route::get('/alunos', [AlunosController::class, 'index'])->name('alunos.index');
+    Route::get('/alunos/criar', [AlunosController::class, 'create'])->name('alunos.create');
+    Route::post('/alunos', [AlunosController::class, 'store'])->name('alunos.store');
+    Route::get('/alunos/{id}/editar', [AlunosController::class, 'edit'])->name('alunos.edit');
+    Route::put('/alunos/{id}', [AlunosController::class, 'update'])->name('alunos.update');
+    Route::delete('/alunos/{id}', [AlunosController::class,'destroy'])->name('alunos.destroy');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/livros', [LivrosController::class, 'index'])->name('livros.index');
+    Route::get('/livros/criar', [LivrosController::class, 'create'])->name('livros.create');
+    Route::post('/livros', [LivrosController::class, 'store'])->name('livros.store');
+    Route::get('/livros/{id}/editar', [LivrosController::class, 'edit'])->name('livros.edit');
+    Route::put('/livros/{id}', [LivrosController::class, 'update'])->name('livros.update');
+    Route::delete('/livros/{id}', [LivrosController::class, 'destroy'])->name('livros.destroy');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+});
